a2d05d649b8c065ac9d031f99417e98a88213c70
jeissonmgz <jeissonmgz@gmail.com>
2023-01-29 20:22:08 -0500

    :sparkles: Share same html template

--- src/server/index.js
+++ src/server/index.js
@@ -9,18 +9,30 @@ import App from '../shared/App'
 import routes from '../shared/routing/serverRoutes'
 
 const app = express()
+const { readFile } = require('fs/promises')
 
 app.use(cors())
 app.use(express.static('dist'))
 
-app.get('*', (req, res, next) => {
+function content(path) {
+  return readFile(path, 'utf8')
+}
+
+const renderApp = (req, res, next) => {
+
   const activeRoute = routes.find((route) => matchPath(route.path, req.url)) || {}
 
-  const promise = activeRoute.fetchInitialData
+  const getQuery = activeRoute.fetchInitialData
     ? activeRoute.fetchInitialData(req.path)
     : Promise.resolve()
 
-  promise.then((data) => {
+  const getHtmlTemplate = content('./dist/index.html')
+  Promise.all(
+    [
+      getQuery,
+      getHtmlTemplate
+    ]
+  ).then(([data, htmlTemplate]) => {
     console.log('SSR: redering...')
     const markup = ReactDOM.renderToString(
       <StaticRouter location={req.url} >
@@ -28,23 +40,24 @@ app.get('*', (req, res, next) => {
       </StaticRouter>
     )
 
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <link rel="icon" type="image/x-icon" href="https://web.dev/images/favicon.ico">
-          <title>SSR with React Router</title>
-          <script src="/bundle.js" defer></script>
-          <link href="/main.css" rel="stylesheet">
-          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-        </head>
+    let htmlRedered = htmlTemplate.replace('<!--initialdata-->', `<script>window.__INITIAL_DATA__ = ${serialize(data)}</script>`).replace('<!--markup-->', markup);
 
-        <body>
-          <div id="app">${markup}</div>
-        </body>
-      </html>
-    `)
+    res.send(htmlRedered);
   }).catch(next)
+}
+
+app.get('/popular/ruby', (req, res, next) => {
+  renderApp(req, res, next)
+})
+
+app.get('*', (_req, res, next) => {
+  console.log('Here return bundle')
+  const getHtmlTemplate = content('./dist/index.html')
+  getHtmlTemplate.then((htmlTemplate) => {
+    res.send(htmlTemplate);
+  }).catch(next)
+
+
 })
 
 const PORT = process.env.PORT || 3000
